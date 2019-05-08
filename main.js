ae5473299392c61bad481cdfd5d9d69233f29f73
RoB8080 <75566977@qq.com>
2019-05-08 13:45:45 +0800

    update ui

--- main.js
+++ main.js
@@ -68,39 +68,39 @@ class Agent {
 }
 function initAgents() {
     //六星
-    new Agent("能天使",6,[1,12,22,23]);
-    new Agent("伊芙利特",6,[2,12,22,30,32]);
-    new Agent("推进之王",6,[3,12,21,23,28]);
-    new Agent("银灰",6,[4,11,21,23,27]);
-    new Agent("星熊",6,[5,12,21,23,24]);
-    new Agent("塞雷娅",6,[5,12,21,24,26,27]);
-    new Agent("闪灵",6,[6,12,22,26,27]);
-    new Agent("夜莺",6,[6,12,22,26,27]);
+    new Agent("能天使",6,[1,12,22,23,39]);
+    new Agent("伊芙利特",6,[2,12,22,30,32,39]);
+    new Agent("推进之王",6,[3,12,21,23,28,39]);
+    new Agent("银灰",6,[4,11,21,23,27,39]);
+    new Agent("星熊",6,[5,12,21,23,24,39]);
+    new Agent("塞雷娅",6,[5,12,21,24,26,27,39]);
+    new Agent("闪灵",6,[6,12,22,26,27,39]);
+    new Agent("夜莺",6,[6,12,22,26,27,39]);
     //五星
-    new Agent("普罗旺斯",5,[1,12,22,23]);
-    new Agent("蓝毒",5,[1,12,22,23]);
-    new Agent("守林人",5,[1,12,22,23,36]);
-    new Agent("白金",5,[1,12,22,23]);
-    new Agent("陨星",5,[1,12,22,30,32]);
-    new Agent("德克萨斯",5,[3,12,21,28,34]);
-    new Agent("凛冬",5,[3,12,21,27,28]);
-    new Agent("芙兰卡",5,[4,12,21,23,25]);
-    new Agent("幽灵鲨",5,[4,12,21,25,30]);
-    new Agent("因陀罗",5,[4,12,21,23,25]);
-    new Agent("临光",5,[5,12,21,24,26]);
-    new Agent("雷蛇",5,[5,12,21,23,24]);
-    new Agent("火神",5,[5,12,21,23,24,25]);
-    new Agent("可颂",5,[5,12,21,24,35]);
-    new Agent("白面鸮",5,[6,12,22,26,27]);
-    new Agent("赫默",5,[6,12,22,26]);
-    new Agent("华法琳",5,[6,12,22,26,27]);
-    new Agent("初雪",5,[7,12,22,32]);
-    new Agent("真理",5,[7,12,22,23,33]);
-    new Agent("梅尔",5,[7,12,22,31,34]);
-    new Agent("红",5,[8,12,21,29,34]);
-    new Agent("狮蝎",5,[8,12,21,23,25]);
-    new Agent("崖心",5,[8,12,21,23,35]);
-    new Agent("食铁兽",5,[8,12,21,33,35]);
+    new Agent("普罗旺斯",5,[1,12,22,23,38]);
+    new Agent("蓝毒",5,[1,12,22,23,38]);
+    new Agent("守林人",5,[1,12,22,23,36,38]);
+    new Agent("白金",5,[1,12,22,23,38]);
+    new Agent("陨星",5,[1,12,22,30,32,38]);
+    new Agent("德克萨斯",5,[3,12,21,28,34,38]);
+    new Agent("凛冬",5,[3,12,21,27,28,38]);
+    new Agent("芙兰卡",5,[4,12,21,23,25,38]);
+    new Agent("幽灵鲨",5,[4,12,21,25,30,38]);
+    new Agent("因陀罗",5,[4,12,21,23,25,38]);
+    new Agent("临光",5,[5,12,21,24,26,38]);
+    new Agent("雷蛇",5,[5,12,21,23,24,38]);
+    new Agent("火神",5,[5,12,21,23,24,25,38]);
+    new Agent("可颂",5,[5,12,21,24,35,38]);
+    new Agent("白面鸮",5,[6,12,22,26,27,38]);
+    new Agent("赫默",5,[6,12,22,26,38]);
+    new Agent("华法琳",5,[6,12,22,26,27,38]);
+    new Agent("初雪",5,[7,12,22,32,38]);
+    new Agent("真理",5,[7,12,22,23,33,38]);
+    new Agent("梅尔",5,[7,12,22,31,34,38]);
+    new Agent("红",5,[8,12,21,29,34,38]);
+    new Agent("狮蝎",5,[8,12,21,23,25,38]);
+    new Agent("崖心",5,[8,12,21,23,35,38]);
+    new Agent("食铁兽",5,[8,12,21,33,35,38]);
     //四星
     new Agent("白雪",4,[1,12,22,30,33]);
     new Agent("流星",4,[1,12,22,23,32]);
@@ -161,13 +161,11 @@ class Combination {
                 if(sixStar&&(e.star===6)){
                     possibleAgents.push(e);
                     starCount[6]++;
-                    selectedTags=[39];
                     maxStar=6;minStar=6;
                 }
                 else if (fiveStar&&(e.star===5)){
                     possibleAgents.push(e);
                     starCount[5]++;
-                    selectedTags=[38];
                     maxStar=5;minStar=5;
                 }
                 else if ((!fiveStar&&!sixStar)&&e.star<6) {
@@ -187,7 +185,7 @@ class Combination {
             temp.push(e);
         });
         this.selectedTags=temp;
-        this.score=-(starCount[3]/possibleAgents.length)+(starCount[4]/possibleAgents.length)+(starCount[5]/possibleAgents.length)*3+(starCount[6]/possibleAgents.length)*100+(temp.length-1)*0.05;
+        this.score=(-(starCount[3]/possibleAgents.length)+(starCount[4]/possibleAgents.length)+(starCount[5]/possibleAgents.length)*4+(starCount[6]/possibleAgents.length)*100)+(temp.length-1)*0.05;
         return this;
     }
 }
@@ -209,36 +207,55 @@ function calculate() {
         else if(e===39)
             sixStarTag = true;
     });
-    let combinations=[];//初始化标签组合数组
+    let combinations=[],t=[];//初始化标签组合数组
     if(fiveStarTag||sixStarTag){
-        combinations.push(new Combination([],fiveStarTag,sixStarTag))
+        t.push(sixStarTag?39:38);
+        combinations.push(new Combination(t,fiveStarTag,sixStarTag));//只有星级标签情况
+        console.log(t);
+        console.log(combinations);
+        normalTag.forEach(function(e){
+            normalTag.forEach(function(u){u[1]=true});//重置使用位
+            t.push(e[0]);//加入第二个tag
+            combinations.push(new Combination(t,fiveStarTag,sixStarTag));//单一般标签情况
+            e[1]=false;//可用位置false
+            e[2]=false;//顺序可用位置false
+            normalTag.forEach(function(f){
+                if(f[1]&&f[2]){
+                    t.push(f[0]);//加入第三个tag
+                    combinations.push(new Combination(t,fiveStarTag,sixStarTag));//双一般标签情况
+                    f[1]=false;//可用位置false
+                    t.pop();//还原
+                }
+            });
+            t.pop();//还原
+        });
+        t.pop();//还原
     }
-    normalTag.forEach(function(e){
-        let t=[];
-        normalTag.forEach(function(u){u[1]=true});//重置使用位
-        t.push(e[0]);//加入第一个tag
-        combinations.push(new Combination(t,fiveStarTag,sixStarTag));//单一般标签情况
-        e[1]=false;//可用位置false
-        e[2]=false;//顺序可用位置false
-        normalTag.forEach(function(f){
-            if(f[1]&&f[2]){
-                t.push(f[0]);//加入第二个tag
-                combinations.push(new Combination(t,fiveStarTag,sixStarTag));//双一般标签情况
-                f[1]=false;//可用位置false
-                if(!(sixStarTag||fiveStarTag)) {
+    else {
+        normalTag.forEach(function(e){
+            normalTag.forEach(function(u){u[1]=true});//重置使用位
+            t.push(e[0]);//加入第一个tag
+            combinations.push(new Combination(t,false,false));//单一般标签情况
+            e[1]=false;//可用位置false
+            e[2]=false;//顺序可用位置false
+            normalTag.forEach(function(f){
+                if(f[1]&&f[2]){
+                    t.push(f[0]);//加入第二个tag
+                    combinations.push(new Combination(t,false,false));//双一般标签情况
+                    f[1]=false;//可用位置false
                     normalTag.forEach(function(g) {
                         if (g[1]&&g[2]) {
                             t.push(g[0]);//加入第三个tag
-                            combinations.push(new Combination(t,fiveStarTag,sixStarTag));//三一般标签情况
+                            combinations.push(new Combination(t,false,false));//三一般标签情况
                             t.pop();//还原
                         }
-                    })
+                    });
+                    t.pop();//还原
                 }
-                t.pop();//还原
-            }
+            });
+            t.pop();//还原
         });
-        t.pop();//还原
-    });
+    }
     //处理得到的组合
     let best=[],highscore=[0,0,0];
     combinations.forEach(function(e){
